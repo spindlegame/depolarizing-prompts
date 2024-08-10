ae2b6e28df2522c349ace432f0cc621dd6582ddc
Tabea Hirzel <3715760+thirzel@users.noreply.github.com>
2024-08-10 13:03:11 +0200

    Update fetch_messages.py

--- python/Social_media_scrapers/Telegram_API/AlJazeera_Commentary_Bot/fetch_messages.py
+++ python/Social_media_scrapers/Telegram_API/AlJazeera_Commentary_Bot/fetch_messages.py
@@ -5,14 +5,14 @@ api_id = '27931942'
 api_hash = '2f1d6cfb0a9b788b73491cfa27e489ba'
 bot_token = '7334856578:AAHMIeACt6gd1i7rDUQEN-NgKzKYqFBRMEs'
 
-# Source channel and target bot channel
-source_channel = 'https://t.me/aljazeera_world'
+# List of source channels and target bot channel
+source_channels = ['https://t.me/aljazeera_world', 'https://t.me/BBCWorldoffl', 'https://t.me/palki_sharma']
 target_channel = '@AlJazeera_Commentary_Bot'
 
 # Initialize the client
 client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
 
-@client.on(events.NewMessage(chats=source_channel))
+@client.on(events.NewMessage(chats=source_channels))
 async def handler(event):
     # Forward the message to your bot's channel
     await client.send_message(target_channel, event.message)
