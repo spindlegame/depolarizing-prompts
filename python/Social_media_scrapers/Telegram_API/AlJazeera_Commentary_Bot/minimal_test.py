7443481e3548fc1830548b843b2064c06b61812b
Tabea Hirzel <3715760+thirzel@users.noreply.github.com>
2024-08-11 07:47:11 +0200

    Update minimal_test.py

--- python/Social_media_scrapers/Telegram_API/AlJazeera_Commentary_Bot/minimal_test.py
+++ python/Social_media_scrapers/Telegram_API/AlJazeera_Commentary_Bot/minimal_test.py
@@ -1,12 +1,12 @@
 from telegram import Bot
 
 # Replace with your bot token
-bot_token = 'YOUR_BOT_TOKEN'
+bot_token = ''7334856578:AAHMIeACt6gd1i7rDUQEN-NgKzKYqFBRMEs'
 
 # Initialize the bot
 bot = Bot(token=bot_token)
 
 # Send a simple message to yourself (replace with your chat ID)
-bot.send_message(chat_id='YOUR_CHAT_ID', text="Testing bot response")
+bot.send_message(chat_id='1838982250', text="Testing bot response")
 
 print("Message sent")
